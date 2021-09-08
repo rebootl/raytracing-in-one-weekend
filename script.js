461e43a7a8d914c95a3b1c586f38e8ba7162c7e2
Glitch (glitch-hello-website) <none>
2021-09-08 10:07:57 +0000

    🚅🏦 Checkpoint ./script.js:8391582/38 ./index.html:8391582/14

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
+const c = document.getElementById("mycanvas"); 
+const ctx = c.getContext("2d");
+ 
+const width = c.width;
+const height = c.height;
+ 
+const imagedata = ctx.createImageData(width, height);
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+for (let y = 0; y < height; y++) {
+  //console.log('line: ' + (y+1))
+  for (let x = 0; x < width; x++) {
+    const pixelindex = (y * width + x) * 4;
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
+    const red = x / (width - 1);
+    const green = (255 - y) / (height - 1);
+    const blue = 0.25;
+    
+    imagedata.data[pixelindex] = parseInt(red * 255.999);
+    imagedata.data[pixelindex+1] = parseInt(green * 255.999);
+    imagedata.data[pixelindex+2] = parseInt(blue * 255.999);
+    imagedata.data[pixelindex+3] = 255;
+  }
 }
 
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+ctx.putImageData(imagedata, 0, 0);
